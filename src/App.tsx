e8d67101f5867dfd2dcfaaafb5c8dec94fbe1e9f
sofiaisreis <fifareis@gmail.com>
2022-08-25 20:39:31 +0100

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import { Container } from "@mantine/core";
+
+function App() {
+  return <Container>Default container</Container>;
+}
+
+export default App;
